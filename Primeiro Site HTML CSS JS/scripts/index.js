8ce47328319b82c54c51bb0845f876794c582432
joaogw21 <167151894+joaogw21@users.noreply.github.com>
2024-09-18 14:57:29 -0300

    Add files via upload

---
+++ Primeiro Site HTML CSS JS/scripts/index.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Seleciona o botão e o container pelo ID e classe
+    const button = document.getElementById('toggleButton');
+    const container = document.querySelector('container');
+    
+    addEventListener('click', esconder);
+
+    function esconder(container) {
+        container == 'on'
+    } if (container == 'on'){
+        this.styleSheets == 'display:none;'
+    }
+    
+  });
+  
